847d84dde45743dbb56219391826689fe11a08e7
Mrunali Gadekar <mrunaligadekar191@gmail.com>
2023-09-21 13:45:22 +0530

    Update About.js
    About us updated

--- src/components/About.js
+++ src/components/About.js
@@ -34,7 +34,6 @@ export default function About() {
 
   return (
     <div className="container" style={mystyle}>
-        <h1 className="my-3">About Us</h1>
         <div className="accordion my-3" id="accordionExample">
         <div className="accordion-item ">
         <h2 className="accordion-header" id="headingOne">
